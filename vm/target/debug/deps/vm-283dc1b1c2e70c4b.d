b026d4944f6b9a785c77f1b63350639c5b8c9b1b
mumiso <zkayrodu@gmail.com>
2023-11-30 02:28:27 +0900

    remove some files

--- vm/target/debug/deps/vm-283dc1b1c2e70c4b.d
+++
@@ -1,7 +0,0 @@
-/root/rust/nand2tetris/vm/target/debug/deps/vm-283dc1b1c2e70c4b.rmeta: src/main.rs src/code_writer.rs src/parser.rs
-
-/root/rust/nand2tetris/vm/target/debug/deps/vm-283dc1b1c2e70c4b.d: src/main.rs src/code_writer.rs src/parser.rs
-
-src/main.rs:
-src/code_writer.rs:
-src/parser.rs:
