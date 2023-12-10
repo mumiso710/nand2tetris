a8a08fa73f94f6a5d52bcc221573135e4df0ca53
mumiso710 <zkayrodu@gmail.com>
2023-12-10 16:03:36 +0900

    change to be able to manupulate directory

--- vm/src/main.rs
+++ vm/src/main.rs
@@ -10,29 +10,43 @@ mod parser;
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let in_filename = &args[1];
-    let out_filename = in_filename.replace("vm", "asm");
+    // let file_name = &args[1];
+    let dir_name = &args[1];
 
-    let mut parser = Parser::new(&in_filename).unwrap();
-    let mut code_writer = CodeWriter::new(&out_filename).unwrap();
+    match extrac_vm_files(dir_name) {
+        Ok(vm_files) => {
+            for file_name in vm_files {
+                let file_name = file_name.to_str().unwrap();
+                let out_filename = file_name.replace("vm", "asm");
 
-    while parser.has_more_commands() {
-        parser.advance();
-        match parser.command_type() {
-            CommandType::CArithmetic => code_writer.write_arithmetic(&parser.command()),
-            CommandType::CPush | CommandType::CPop => {
-                code_writer.write_push_pop(parser.command_type(), &parser.arg1(), parser.arg2())
+                let mut parser = Parser::new(&file_name).unwrap();
+                let mut code_writer = CodeWriter::new(&out_filename).unwrap();
+
+                while parser.has_more_commands() {
+                    parser.advance();
+                    match parser.command_type() {
+                        CommandType::CArithmetic => code_writer.write_arithmetic(&parser.command()),
+                        CommandType::CPush | CommandType::CPop => code_writer.write_push_pop(
+                            parser.command_type(),
+                            &parser.arg1(),
+                            parser.arg2(),
+                        ),
+                        CommandType::CGoto => code_writer.write_goto(&parser.arg1()),
+                        CommandType::CLabel => code_writer.write_label(&parser.arg1()),
+                        CommandType::CIf => code_writer.write_if(&parser.arg1()),
+                        CommandType::CFunction => {
+                            code_writer.write_function(&parser.arg1(), parser.arg2())
+                        }
+                        CommandType::CReturn => code_writer.write_return(),
+                        CommandType::CCall => code_writer.write_call(&parser.arg1(), parser.arg2()),
+                    }
+                }
+
+                println!("{:?}", parser.commands);
             }
-            CommandType::CGoto => code_writer.write_goto(&parser.arg1()),
-            CommandType::CLabel => code_writer.write_label(&parser.arg1()),
-            CommandType::CIf => code_writer.write_if(&parser.arg1()),
-            CommandType::CFunction => code_writer.write_function(&parser.arg1(), parser.arg2()),
-            CommandType::CReturn => code_writer.write_return(),
-            CommandType::CCall => code_writer.write_call(&parser.arg1(), parser.arg2()),
         }
+        Err(err) => eprintln!("Error: {}", err),
     }
-
-    println!("{:?}", parser.commands);
 }
 
 fn extrac_vm_files(dir_name: &str) -> Result<Vec<PathBuf>, std::io::Error> {
