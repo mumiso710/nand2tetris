bc17c23c1637db35e8caa29d7a08605f46e23094
mumiso <zkayrodu@gmail.com>
2023-12-19 16:41:12 +0900

    refactor function to read filename from argument

--- jack_analyzer/src/main.rs
+++ jack_analyzer/src/main.rs
@@ -1,17 +1,24 @@
 mod jack_tokenizer;
 
-use std::env;
+use std::{env, ops::Deref, process};
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
+    let target_name = get_target();
 
-    let target_name = &args[1];
-
-    if is_file(target_name) {
+    if is_file(&target_name) {
         println!("is file");
     } else {
         println!("is not file");
     }
+}
+
+fn get_target() -> String {
+    let args: Vec<String> = env::args().collect();
+    let target_name = args.get(1).unwrap_or_else(|| {
+        eprintln!("not enough argumens");
+        process::exit(1);
+    });
+    String::from(target_name)
 }
 
 fn is_file(target_name: &str) -> bool {
